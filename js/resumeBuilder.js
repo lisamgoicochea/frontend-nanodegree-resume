32a0ddd4ec1c5e09b41ea33ccf98dbb41e40d2cc
Lisa Marie Goicochea <lisamgoicochea@gmail.com>
2018-01-31 12:59:29 -0500

    added selfie and job locations

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,12 +8,12 @@ var bio = {
  		email: 'lisamgoicochea@gmail.com',
  		github: 'lisamgoicochea',
  		twitter: '@lisamgoicochea',
- 		location: 'New York'
+ 		location: 'New York, New York'
  },
  welcomeMessage: 'lorem ipsum dolor sit amet etc etc etc.',
  skills: [
  	"HTML", 'CSS', 'JavaScript'],
- 	bioPic: 'images/me.jpg'
+ 	bioPic: 'images/winterselfie.jpg',
  	};
 
  	bio.display = function() {
@@ -23,7 +23,7 @@ var bio = {
 
  		var formattedMobile = HTMLmobile.replace(DATA, bio.contacts.mobile);
  		var formattedEmail = HTMLemail.replace(DATA, bio.contacts.email);
- 		var formattedGithub = HTMLgithub.replace(DATA. bio.contacts.github);
+ 		var formattedGithub = HTMLgithub.replace(DATA, bio.contacts.github);
  		var formattedTwitter = HTMLtwitter.replace(DATA, bio.contacts.twitter);
  		var formattedLocation = HTMLlocation.replace(DATA, bio.contacts.location);
 
@@ -48,63 +48,66 @@ var bio = {
  	};
 
 var work = {
- 	jobs: [
+	jobs: [
  		{
  			employer: 'Self Employed',
  			title: 'Entrepreneur',
- 			dates: 'May 2016 - Future',
+			dates: 'May 2016 - Future',
+			location: 'Chicago, Illinois',
  			description: 'Working Hard While Hardly Working', 
  		},
  		{
  			employer: "Lerner Cumbo and Associates",
- 			"title": "Placement Counselor",
- 			"dates": "October 2015 - August 2016",
- 			"description": "fill this in later"
- 		}
-	]
-};
+ 			title: "Placement Counselor",
+			 dates: "October 2015 - August 2016",
+			 location: 'Los Angeles, California',
+ 			description: 'Graciously place candidates in roles they qualify for',
+ 		};
+	];
 
- "display": funtion() {
- 	if (work.jobs.length > 0) {
- 		for (var i = 0, len = work.jobs.length; i < len; i++) {
- 			var formattedEmployer = HTMLworkEmployer.replace(DATA, work.jobs[i].employer);
- 			var formattedTitle = HTMLworkTitle.replace(DATA, work.jobs[i].title);
- 			var formattedDates = HTMLworkDates.replace(DATA, work.jobs[i].dates);
- 			var formattedLocation = HTMLworkLocation.replace(DATA, work.jobs[i].location);
- 			var formattedDescription = HTMLworkDescription.replace(DATA, work.jobs[i].description);
- 			var formattedEmployerTitle = formattedEmployer + formattedTitle;
+	"display": function() {
+		if (work.jobs.length > 0) {
+			for (var i = 0, len = work.jobs.length; i < len; i++) {
+				var formattedEmployer = HTMLworkEmployer.replace(DATA, work.jobs[i].employer);
+				var formattedTitle = HTMLworkTitle.replace(DATA, work.jobs[i].title);
+				var formattedDates = HTMLworkDates.replace(DATA, work.jobs[i].dates);
+				var formattedLocation = HTMLworkLocation.replace(DATA, work.jobs[i].location);
+				var formattedDescription = HTMLworkDescription.replace(DATA, work.jobs[i].description);
+				var formattedEmployerTitle = formattedEmployer + formattedTitle;
 
- 			$('#workExperience').append(HTMLworkStart);
- 			$('.work-entry:last').append(formattedEmployerTitle);
- 			$('.work-entry:last').append(formattedDates);
- 			$('.work-entry:last').append(formattedLocation);
- 			$('.work-entry:last').append(formattedDescription);
- 		}
- 	}
- }
+				$('#workExperience').append(HTMLworkStart);
+				$('.work-entry:last').append(formattedEmployerTitle);
+				$('.work-entry:last').append(formattedDates);
+				$('.work-entry:last').append(formattedLocation);
+				$('.work-entry:last').append(formattedDescription);
+			}
+		}
+	}
 };
 
 var projects = {
-	"projects": [
+	projects: [
 		{
-			title: 'Sample Project 1',
-			"dates": "2014",
-			"description": "fill this in later",
-			"images": [
+			title: 'Princess Run: Classic Arcade Game',
+			dates: "2018",
+			description: "Get the Princess from one side to the other without bumping into any enemy bugs!",
+			images: [
 			"insert image here", 
 			"insert another image here"
 			]
 		}
-	]
-};
-"display":  function() {
-	if (projects.projects.length > 0) {
-		for (var i = 0, iLen = projects.projects.length; i < iLen; i++) {
-			var formattedTitle = HTMLprojectTitle.replace(DATA, projects.projects[i].title);
-			var formattedTitle = HTMLprojectTitle.replace(DATA, projects.projects[i].dates);
-			var formattedDescription = HTMLprojectDescription.replace(DATA, projects.projects[i].description);
+	],
+
+	"display":  function() {
+		if (projects.projects.length > 0) {
+			for (var i = 0, iLen = projects.projects.length; i < iLen; i++) {
+				var formattedTitle = HTMLprojectTitle.replace(DATA, projects.projects[i].title);
+				var formattedDates = HTMLprojectTitle.replace(DATA, projects.projects[i].dates);
+				var formattedDescription = HTMLprojectDescription.replace(DATA, projects.projects[i].description);
+			}
 		}
-	}	
+	}
+};	
 
 
  var education = {
@@ -129,39 +132,40 @@ var projects = {
 		dates: '2017',
 		url: 'http://www.udacity.com/course/ud804'
 		}
-	]
-};
+	],
 
-"display": function () {
-	if (education.schools.length > 0 || education.onlineCourses.length > 0) {
-		for (var i = 0, iLen = education.schools.length; i < iLen; i++) {
-			var formattedName = HTMLschoolName.replace(DATA, education.schools[i].name);
-			var formattedDegree = HTMLschoolDegree.replace(DATA, education.schools[i].degree);
-			var formattedSchoolDates = HTMLschoolDates.replace(DATA, education.schools[i].dates);
-			var formattedLocation = HTMLschoolLocation.replace(DATA, education.schools[i].location);
-			var formattedNameDegree = formattedName + formattedDegree;
 
-			$('#education').append(HTMLschoolStart);
-			$('.education-entry:last').append(formattedNameDegree);
-			$('.education-entry:last').append(formattedSchoolDates);
-			$('.education-entry:last').append(formattedLocation);
+	"display": function () {
+		if (education.schools.length > 0 || education.onlineCourses.length > 0) {
+			for (var i = 0, iLen = education.schools.length; i < iLen; i++) {
+				var formattedName = HTMLschoolName.replace(DATA, education.schools[i].name);
+				var formattedDegree = HTMLschoolDegree.replace(DATA, education.schools[i].degree);
+				var formattedSchoolDates = HTMLschoolDates.replace(DATA, education.schools[i].dates);
+				var formattedLocation = HTMLschoolLocation.replace(DATA, education.schools[i].location);
+				var formattedNameDegree = formattedName + formattedDegree;
 
-			for (var j = 0, jLen = education.schools[i].majors.length; j < jLen; j++) {
-				var formattedMajor = HTMLschoolMajor.replace(DATA, education.schools[i].majors[j]);
+				$('#education').append(HTMLschoolStart);
+				$('.education-entry:last').append(formattedNameDegree);
+				$('.education-entry:last').append(formattedSchoolDates);
+				$('.education-entry:last').append(formattedLocation);
 
-			$('#education-entry:last').append(formattedMajor);
+				for (var j = 0, jLen = education.schools[i].major.length; j < jLen; j++) {
+					var formattedMajor = HTMLschoolMajor.replace(DATA, education.schools[i].major[j]);
+
+				$('#education-entry:last').append(formattedMajor);
+				}
+			}
+
+			$('#education').append(HTMLonlineClasses);
+
+			for (var k = 0, len = education.onlineCourses.length; k < len; k++) {
+				var formattedTitle = HTMLonlineTitle.replace(DATA, education.onlineCourses[k].title).replace('#', education.onlineCourses[k].url);
+				var formattedSchool = HTMLonlineSchool.replace(DATA, education.onlineCourses[k].school);
+				var formattedOnlineDates = HTMLonlineDates.replace(DATA, education.onlineCourses[k].dates);
 			}
 		}
-
-		$('#education').append(HTMLonlineClasses);
-
-		for (var k = 0, len = education.onlineCourses.length; k < len; k++) {
-			var formattedTitle = HTMLonlineTitle.replace(DATA, education.onlineCourses[k].title).replace('#', education.onlineCourses[k].url);
-			var formattedSchool = HTMLonlineSchool.replace(DATA, education.onlineCourses[k].school);
-			var formattedOnlineDates = HTMLonlineDates.replace(DATA, )
-		}
 	}
-};
+ };
 
 bio.display();
 work.display();
